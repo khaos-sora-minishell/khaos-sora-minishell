096171450f9b4ad47e052830276f0b01afc7b2c8
suat kıvam <suatkivam@gmail.com>
2025-12-06 15:08:54 +0300

    env part added need norm

--- env/env_list.c
+++ env/env_list.c
@@ -10,15 +10,81 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "minishell.h"
+
+/* create new a env node*/
+static t_env	*create_env_node(char *key, char *value, void *arena)
+{
+	t_env	*node;
+
+	node = gc_malloc(gc_get_current(), sizeof(t_env));
+	if (!node)
+		return (NULL);
+	node->key = key;
+	node->value = value;
+	node->next = NULL;
+	return (node);
+}
+
+/*add element of list end  */
+static void	env_add_back(t_env **head, t_env *new_node)
+{
+	t_env	*temp;
+
+	if (!*head)
+	{
+		*head = new_node;
+		return ;
+	}
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
+}
+
+/* pars to "KEY=VALUE" string and add list*/
+static void	process_env_str(t_env **head, char *env_str, void *arena)
+{
+	char			*eq_pos;
+	char			*key;
+	char			*value;
+	t_env			*node;
+	t_gc_context	*contex;
+
+	contex = gc_get_current(); // arena parameter maybe
+	eq_pos = ft_strchr(env_str, '=');
+	if (eq_pos)
+	{
+		key = gc_strndup(contex, env_str, eq_pos - env_str);
+		value = gc_strdup(contex, eq_pos + 1);
+	}
+	else
+	{
+		key = gc_strdup(contex, env_str);
+		value = NULL; // EXPORT edilmiş ama değeri yok
+	}
+	node = create_env_node(key, value, arena);
+	if (node)
+		env_add_back(head, node);
+}
 
 /*
  * Environment linked list operations
  */
 t_env	*init_env(char **envp, void *arena)
 {
-	(void)envp;
-	(void)arena;
-	// TODO: Implement env list initialization
-	return (NULL);
+	t_env	*head;
+	int		i;
+
+	head = NULL;
+	i = 0;
+	if (!envp)
+		return (NULL);
+	while (envp[i])
+	{
+		process_env_str(head, envp[i], arena);
+		i++;
+	}
+	return (head);
 }
--- env/env_utils.c
+++ env/env_utils.c
@@ -11,15 +11,19 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "utils.h"
 
 /*
  * Get environment variable value
  */
 char	*get_env_value(t_env *env, char *key)
 {
-	(void)env;
-	(void)key;
-	// TODO: Implement get env value
+	while (env)
+	{
+		if (ft_strcmp(env->key, key) == 0)
+			return (env->value);
+		env = env->next;
+	}
 	return (NULL);
 }
 
@@ -28,11 +32,26 @@ char	*get_env_value(t_env *env, char *key)
  */
 void	set_env_value(t_env **env, char *key, char *value, void *arena)
 {
-	(void)env;
-	(void)key;
-	(void)value;
-	(void)arena;
-	// TODO: Implement set env value
+	t_env			*curr;
+	t_env			*new_node;
+	t_gc_context	*contex;
+
+	contex = gc_get_current();
+	curr = *env;
+	while ((curr))
+	{
+		if (ft_strcmp(curr->key, key) == 0)
+		{
+			curr->value = gc_strdup(contex, value);
+			return ;
+		}
+		curr = curr->next;
+	}
+	new_node = gc_malloc(contex, sizeof(t_env));
+	new_node->key = gc_strdup(contex, key);
+	new_node->value = gc_strdup(contex, value);
+	new_node->next = *env;
+	*env = new_node;
 }
 
 /*
@@ -40,7 +59,54 @@ void	set_env_value(t_env **env, char *key, char *value, void *arena)
  */
 void	unset_env_value(t_env **env, char *key)
 {
-	(void)env;
-	(void)key;
-	// TODO: Implement unset env value
+	t_env	*curr;
+	t_env	*prev;
+
+	curr = *env;
+	prev = NULL;
+	while (curr)
+	{
+		if (ft_strcmp(curr->key, key) == 0)
+		{
+			if (prev)
+				prev->next = curr->next;
+			else
+				*env = curr->next;
+			return ;
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+}
+
+char	**env_list_to_array(t_env *env, void *arena)
+{
+	t_env			*tmp;
+	char			**array;
+	int				count;
+	int				i;
+	t_gc_context	*contex;
+	char			*temp_str;
+
+	contex = gc_get_current();
+	count = 0;
+	tmp = env;
+	while (tmp && ++count)
+		tmp = tmp->next;
+	array = gc_malloc(contex, sizeof(char *) * (count + 1));
+	if (!array)
+		return (NULL);
+	i = 0;
+	tmp = env;
+	while (tmp)
+	{
+		if (tmp->value)
+		{
+			temp_str = gc_strjoin(contex, tmp->key, "=");
+			array[i++] = gc_strjoin(contex, temp_str, tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	array[i] = NULL;
+	return (array);
 }
