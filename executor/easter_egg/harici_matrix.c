0b595dd648c3b077d3bfeae9c734312d188bb645
suat kıvam <suatkivam@gmail.com>
2025-12-22 14:07:57 +0300

    fix: Update author information and improve memory management in harici_matrix function

--- executor/easter_egg/harici_matrix.c
+++ executor/easter_egg/harici_matrix.c
@@ -3,15 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   harici_matrix.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: harici <harici@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*   By: akivam <akivam@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 13:25:00 by harici            #+#    #+#             */
-/*   Updated: 2025/12/22 13:48:29 by harici           ###   ########.fr       */
+/*   Updated: 2025/12/22 13:57:55 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easteregg.h"
 #include "libft.h"
+#include <readline/readline.h> /* rl_clear_history için */
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/* -- Helper Functions -- */
 
 static unsigned int	simple_rand(unsigned int *seed)
 {
@@ -79,27 +85,60 @@ static void	draw_rain(int *drops, int width, int height, unsigned int *seed)
 	}
 }
 
-void	harici_matrix(char **args, t_shell *shell)
+/* -- Child Process Mantığı -- */
+
+static void	run_matrix_child(t_shell *shell)
 {
 	int				*drops;
 	int				i;
 	unsigned int	seed;
 
-	(void)args;
 	seed = 42;
 	drops = gc_malloc(shell->cmd_arena, sizeof(int) * MATRIX_WIDTH);
 	if (!drops)
-		return ;
+		exit(1);
 	i = -1;
 	while (++i < MATRIX_WIDTH)
 		drops[i] = simple_rand(&seed) % 20;
 	write(1, "\033[2J\033[?25l", 11);
 	i = 0;
-	while (i++ < 150 && get_signal() != SIGINT)
+	while (i++ < 350 && get_signal() != SIGINT)
 	{
 		draw_rain(drops, MATRIX_WIDTH, MATRIX_HEIGHT, &seed);
 		simple_delay();
 	}
 	write(1, "\033[0m\033[2J\033[H\033[?25h", 18);
-	shell->exit_status = 0;
+	/* SOFT CLEANING: Çıkmadan önce child'ın hafızasını temizle */
+	rl_clear_history();              /* Readline geçmişini temizle */
+	gc_destroy(shell->cmd_arena);    /* Command arenayı temizle */
+	gc_destroy(shell->global_arena); /* Global arenayı temizle */
+	exit(0);
+}
+
+/* -- Ana Fonksiyon -- */
+
+void	harici_matrix(char **args, t_shell *shell)
+{
+	pid_t	pid;
+	int		status;
+
+	(void)args;
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("minishell: fork");
+		return ;
+	}
+	if (pid == 0)
+		run_matrix_child(shell);
+	else
+	{
+		ignore_signals();
+		waitpid(pid, &status, 0);
+		setup_signals();
+		if (WIFEXITED(status))
+			shell->exit_status = WEXITSTATUS(status);
+		else if (WIFSIGNALED(status))
+			shell->exit_status = 128 + WTERMSIG(status);
+	}
 }
