abb15a9c629df5443699176d1ff2fa6964416fa3
suat kıvam <suatkivam@gmail.com>
2025-12-22 19:50:26 +0300

    feat: Implement alias and unalias builtins, add color management for terminal prompts

--- executor/easter_egg/easteregg.h
+++ executor/easter_egg/easteregg.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easteregg.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: harici <harici@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*   By: akivam <akivam@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 19:11:12 by akivam            #+#    #+#             */
-/*   Updated: 2025/12/22 13:48:50 by harici           ###   ########.fr       */
+/*   Updated: 2025/12/22 19:13:43 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,23 @@
 
 # define MATRIX_WIDTH 80
 # define MATRIX_HEIGHT 24
+
+#define C_RESET   "\033[0m"
+#define C_RED     "\033[31m"
+#define C_GREEN   "\033[32m"
+#define C_YELLOW  "\033[33m"
+#define C_BLUE    "\033[34m"
+#define C_MAGENTA "\033[35m"
+#define C_CYAN    "\033[36m"
+#define C_WHITE   "\033[37m"
+
+#define BG_RED     "\033[41m"
+#define BG_GREEN   "\033[42m"
+#define BG_YELLOW  "\033[43m"
+#define BG_BLUE    "\033[44m"
+#define BG_MAGENTA "\033[45m"
+#define BG_CYAN    "\033[46m"
+#define BG_BLACK   "\033[40m"
 
 # include "minishell.h"
 
