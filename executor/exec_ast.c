09fa978b49a581589a4adfe555a7f1f4c3323edc
suat kıvam <suatkivam@gmail.com>
2025-12-22 15:59:17 +0300

    feat: refactor exec_ast functions and add test script for command execution

--- executor/exec_ast.c
+++ executor/exec_ast.c
@@ -6,17 +6,16 @@
 /*   By: akivam <akivam@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 21:39:22 by akivam            #+#    #+#             */
-/*   Updated: 2025/12/07 20:44:27 by akivam           ###   ########.fr       */
+/*   Updated: 2025/12/22 15:55:31 by akivam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 #include "minishell.h"
+#include <sys/wait.h>
+#include <unistd.h>
 
-/*
- * Execute subshell in forked process
- */
-static void	execute_subshell(t_ast_node *ast, t_shell *shell)
+static void	exec_subshell(t_ast_node *node, t_shell *shell)
 {
 	pid_t	pid;
 	int		status;
@@ -24,44 +23,46 @@ static void	execute_subshell(t_ast_node *ast, t_shell *shell)
 	pid = fork();
 	if (pid == -1)
 	{
-		perror("fork");
+		perror("minishell: fork");
 		shell->exit_status = 1;
 		return ;
 	}
 	if (pid == 0)
 	{
-		execute_ast(ast->subshell_node, shell);
+		setup_child_signals();
+		execute_ast(node->left, shell);
+		cleanup_shell(shell);
 		exit(shell->exit_status);
 	}
+	ignore_signals();
 	waitpid(pid, &status, 0);
+	setup_signals();
 	if (WIFEXITED(status))
 		shell->exit_status = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		shell->exit_status = 128 + WTERMSIG(status);
 }
 
-/*
- ** Main executor girişi - Parser'dan gelen AST'yi execute eder
- ** B<u fonksiyon main. c'den çağrılır
- */
-void	execute_ast(t_ast_node *ast, t_shell *shell)
+void	execute_ast(t_ast_node *node, t_shell *shell)
 {
-	if (!ast || !shell)
+	if (!node)
 		return ;
-	if (ast->type == NODE_CMD)
-		execute_command(ast->cmd, shell);
-	else if (ast->type == NODE_PIPE)
-		execute_pipe(ast->left, ast->right, shell);
-	else if (ast->type == NODE_AND)
+	if (node->type == NODE_PIPE)
+		execute_pipe(node->left, node->right, shell);
+	else if (node->type == NODE_AND)
 	{
-		execute_ast(ast->left, shell);
+		execute_ast(node->left, shell);
 		if (shell->exit_status == 0)
-			execute_ast(ast->right, shell);
+			execute_ast(node->right, shell);
 	}
-	else if (ast->type == NODE_OR)
+	else if (node->type == NODE_OR)
 	{
-		execute_ast(ast->left, shell);
+		execute_ast(node->left, shell);
 		if (shell->exit_status != 0)
-			execute_ast(ast->right, shell);
+			execute_ast(node->right, shell);
 	}
-	else if (ast->type == NODE_SUBSHELL)
-		execute_subshell(ast, shell);
+	else if (node->type == NODE_SUBSHELL)
+		exec_subshell(node, shell);
+	else
+		execute_command(node->cmd, shell);
 }
